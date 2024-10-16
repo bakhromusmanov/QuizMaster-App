21ec1a09ea1e6125f67f4cc0ec1c42cb2be9c16e
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-10-16 13:52:43 +0500

    Initial Commit

---
+++ QuizMaster/QuizMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizMasterApp.swift
+//  QuizMaster
+//
+//  Created by Bakhrom Usmanov on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
